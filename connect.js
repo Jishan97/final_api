07e212ab472a877cf21e48156b73ca7e4a8a2f19
jishan97 <shaikh.jishan1997@gmail.com>
2019-01-03 20:23:50 +0530

    Adding getById

--- connect.js
+++ connect.js
@@ -34,21 +34,21 @@ app.get('/todos',(req,res)=>{
     .catch(e=>console.log(e))
 })
 
-// app.get('/todos/:id',(req,res)=>{
+app.get('/todos/:id',(req,res)=>{
 
-//     var id = req.params.id;
-//     Todo1.findById(id).then((todo)=>{
-//         if(!todo) {
-//             return res.status(404).send();
-//         }
-//         res.send(todo);
+    var id = req.params.id;
+    Todo1.findById(id).then((todo)=>{
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.send(todo);
 
-//     }).catch((e)=>{
-//         res.status(400).send();
-//     })
+    }).catch((e)=>{
+        res.status(400).send();
+    })
   
     
-// })
+})
 
 
 app.listen(port,()=>{
